d0574d0f39c066128a52ea77209f6c0386515468
Fraser Elliott <fraserelliott@hotmail.com>
2025-07-19 01:30:06 +0100

    added note explaining regex used in password schema

--- routes/users.route.js
+++ routes/users.route.js
@@ -9,6 +9,7 @@ const { validateToken } = require("../middleware/auth.middleware")
 // Define validation rules for user registration inputs
 const userSchema = new FailSchema();
 userSchema.add("username", new StringField().required().minLength(8).maxLength(20).alphanumeric());
+// regex checks for at least one special character from this set !@#$%^&*(),.?":{}|<>_-[]=/+;'/`~\
 userSchema.add("password", new StringField().required().minLength(8).maxLength(20).regex(/[!@#$%^&*(),.?":{}|<>_\-\\[\]=+;'/`~\/]/));
 
 router.post("/", validate(userSchema), usersController.createUser);
